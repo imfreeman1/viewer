bc88a306e79a071249c489d61d146ceef56947db
imfreeman1 <dkfrhkt@gmail.com>
2023-11-20 17:35:27 +0900

    BuggerMenu 추가 및 함수 생성

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import Button from '../Button';
+import BuggerMenu from '../Menu';
 
 const testArr = ['/ About', '/ From', '어쩌구', '저쩌구'];
 
 function Navbar() {
+  const [menuViewer, setMenuViewer] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const viewerHandler = () => {
+    setMenuViewer((s) => !s);
+  };
+  const outsideClickHandler = ({ target }: MouseEvent) => {
+    if (!menuRef.current?.contains(target as Node)) setMenuViewer(false);
+  };
+
+  useEffect(() => {
+    if (menuViewer) {
+      document.addEventListener('mousedown', outsideClickHandler);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', outsideClickHandler);
+    };
+  }, [menuViewer]);
+
   return (
     <div>
       <ul className="md:flex gap-2 hidden ">
@@ -12,9 +34,12 @@ function Navbar() {
           return <li key={navItem}>{navItem}</li>;
         })}
       </ul>
-      <Button className="md:hidden">
-        <AiOutlineMenu />
-      </Button>
+      <div className="w-4">
+        <Button className="md:hidden" onClick={viewerHandler}>
+          {menuViewer ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </Button>
+        <BuggerMenu viewControl={menuViewer} menuRef={menuRef} />
+      </div>
     </div>
   );
 }
