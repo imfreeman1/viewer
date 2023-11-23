524378e2d966810fe9a205fad04ec6c947468018
imfreeman1 <dkfrhkt@gmail.com>
2023-11-23 18:14:46 +0900

    section 태그에 overflow-scroll 추가.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -7,7 +7,7 @@ function Gallery() {
   const imgArr = useAppSelector((state) => state.galleryReducer);
 
   return (
-    <section className="p-6 min-h-full max-h-screen flex justify-center">
+    <section className="p-6 min-h-full max-h-screen flex justify-center overflow-scroll">
       <div className="w-fit h-auto grid grid-cols-3 grid-flow-row justify-items-center gap-4 sm:grid-cols-4">
         {imgArr.map((img) => {
           return (
