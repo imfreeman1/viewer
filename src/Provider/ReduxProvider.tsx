1bb17ac3922fff6e6589e8c5cc40ffbc1299f017
imfreeman1 <dkfrhkt@gmail.com>
2023-11-26 19:07:18 +0900

    import 순서 변경

--- src/Provider/ReduxProvider.tsx
+++ src/Provider/ReduxProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import store from '@/redux/store';
-import React from 'react';
 import { Provider } from 'react-redux';
+import React from 'react';
+import store from '@/redux/store';
 
 function ReduxProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
