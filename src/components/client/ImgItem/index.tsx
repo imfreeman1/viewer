a4f171992a454689835244430ec8c2681511e0f5
imfreeman1 <dkfrhkt@gmail.com>
2023-11-26 19:45:03 +0900

    test용 loaderFunction 제작

--- src/components/client/ImgItem/index.tsx
+++ src/components/client/ImgItem/index.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-import Image from 'next/image';
+import Image, { ImageLoader } from 'next/image';
 import React from 'react';
+
+const imgLoader: ImageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}q=${quality}`;
+};
 
 function ImgItem({ src }: { src: string }) {
   return (
-    <div className="h-20 w-20 sm:h-36 sm:w-36">
-      <Image alt="사진" src={src} width={80} height={80} />
+    <div className="h-20 w-20 sm:h-36 sm:w-36 block">
+      <Image
+        loader={imgLoader}
+        alt="사진"
+        src={src}
+        width={80}
+        height={80}
+        quality={80}
+      />
     </div>
   );
 }
