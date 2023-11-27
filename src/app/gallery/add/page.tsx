256b2cebfa112c57a81525dac1c6ae5c4e336845
imfreeman1 <dkfrhkt@gmail.com>
2023-11-27 18:15:27 +0900

    image ADD 기능 구현

--- src/app/gallery/add/page.tsx
+++ src/app/gallery/add/page.tsx
@@ -1,24 +1,50 @@
 'use client';
 
 import Input from '@/components/client/Input';
-import React, { ChangeEvent } from 'react';
+import Image from 'next/image';
+import React, { ChangeEvent, useState } from 'react';
+
+// const imageLoader = ({src,width,})
 
 function Add() {
-  // const [image, setImage] = useState(null);
+  const [image, setImage] = useState<string | null>(null);
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    // const reader = new FileReader();
-    // reader.onload = (e) => {};
+    const {
+      target: { files },
+    } = e;
+    const file = files![0];
+    const reader = new FileReader();
+
+    reader.onloadend = (loadendEvent: ProgressEvent<FileReader>) => {
+      const currenTarget = loadendEvent.currentTarget as FileReader;
+      const result = currenTarget.result as string;
+      setImage(result);
+    };
+    reader.readAsDataURL(file);
+    // const url = URL.createObjectURL(file);
+    // setImage(url);
   };
   return (
     <section className="section">
       <div>
-        <Input
-          className="oparcity-0"
-          type="file"
-          onChange={(e) => onChange(e)}
-          accept="image/*"
-        />
+        {image ? (
+          <div>
+            <Image
+              className="w-20 h-auto"
+              src={image}
+              alt="loadImg"
+              width={100}
+              height={100}
+            />
+          </div>
+        ) : (
+          <Input
+            className="oparcity-0"
+            type="file"
+            onChange={(e) => onChange(e)}
+            accept="image/*"
+          />
+        )}
       </div>
     </section>
   );
