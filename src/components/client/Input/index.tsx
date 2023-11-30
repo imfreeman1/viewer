48f7a7537febafedf676e465bff9ac8d503092cd
imfreeman1 <dkfrhkt@gmail.com>
2023-11-30 19:27:19 +0900

    className Props 추가.

--- src/components/client/Input/index.tsx
+++ src/components/client/Input/index.tsx
@@ -1,8 +1,8 @@
 import { InputComponentType } from '@/interface/input.interface';
 import React from 'react';
 
-function Input({ onChange, type }: InputComponentType) {
-  return <input onChange={onChange} type={type} />;
+function Input({ className, onChange, type }: InputComponentType) {
+  return <input className={className} onChange={onChange} type={type} />;
 }
 
 export default Input;
