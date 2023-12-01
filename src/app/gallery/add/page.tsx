bc63b0ba4b89b64e854c3c564170036e90919856
imfreeman1 <dkfrhkt@gmail.com>
2023-12-01 18:54:09 +0900

    useImageHook 사용 및 add 기능 , Image Component 반응형 구현

--- src/app/gallery/add/page.tsx
+++ src/app/gallery/add/page.tsx
@@ -2,43 +2,43 @@
 
 import Input from '@/components/client/Input';
 import { AiOutlinePlus } from 'react-icons/ai';
-
 import Image from 'next/image';
-import React, { ChangeEvent, useCallback, useState } from 'react';
+import React from 'react';
 import Card from '@/app/ui/card';
 import Button from '@/components/client/Button';
-
-// const imageLoader = ({src,width,})
+import useImage from '@/hooks/useImage';
+import { useAppDispatch } from '@/hooks/reduxHooks';
+import { addImg } from '@/redux/gallery/action';
+import { useRouter } from 'next/navigation';
 
 function Add() {
-  const [image, setImage] = useState<string | null>(null);
-  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { files },
-    } = e;
-    const file = files![0];
-    const reader = new FileReader();
-
-    reader.onloadend = (loadendEvent: ProgressEvent<FileReader>) => {
-      const currenTarget = loadendEvent.currentTarget as FileReader;
-      const result = currenTarget.result as string;
-      setImage(result);
-    };
-    reader.readAsDataURL(file);
-  }, []);
+  const [image, imageChangeHandler, cancelHandler] = useImage();
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const completeHandler = () => {
+    dispatch(addImg(image as string));
+    router.push('/gallery');
+  };
   return (
     <section className="section flex-col gap-6">
       <Card>
-        <div className=" relative flex justify-center items-center h-full">
+        <div className=" flex justify-center items-center h-full relative z-0">
           {image ? (
-            <Image className="w-20 h-auto" src={image} alt="loadImg" fill />
+            <Image
+              className="w-full h-full"
+              src={image}
+              alt="loadImg"
+              width={700}
+              height={500}
+              sizes="100vw"
+            />
           ) : (
             <>
               <AiOutlinePlus className="w-12 h-auto text-gray-400" />
               <Input
                 className="absolute hover:cursor-copy w-full h-full opacity-0 top-0 left-0"
                 type="file"
-                onChange={(e) => onChange(e)}
+                onChange={(e) => imageChangeHandler(e)}
                 accept="image/*"
               />
             </>
@@ -46,8 +46,18 @@ function Add() {
         </div>
       </Card>
       <div className="w-full flex gap-4 justify-center">
-        <Button className="bg-blue-400">완료</Button>
-        <Button>취소</Button>
+        <Button
+          className="bg-blue-500 hover:bg-blue-400 base-btn"
+          onClick={completeHandler}
+        >
+          완료
+        </Button>
+        <Button
+          className="bg-gray-400 hover:bg-gray-300 base-btn"
+          onClick={cancelHandler}
+        >
+          취소
+        </Button>
       </div>
     </section>
   );
