259595a2fb368c5863d3ea2ccc97a7df16aac8fc
imfreeman1 <dkfrhkt@gmail.com>
2023-12-01 18:56:53 +0900

    imgLoader함수 삭제, 반응형 이미지 구현

--- src/components/client/ImgItem/index.tsx
+++ src/components/client/ImgItem/index.tsx
@@ -1,22 +1,20 @@
 'use client';
 
-import Image, { ImageLoader } from 'next/image';
+import Image from 'next/image';
 import React from 'react';
-
-const imgLoader: ImageLoader = ({ src, width, quality }) => {
-  return `${src}?w=${width}q=${quality}`;
-};
 
 function ImgItem({ src }: { src: string }) {
   return (
-    <div className="h-20 w-20 sm:h-36 sm:w-36 block">
+    <div className="image-container">
       <Image
-        loader={imgLoader}
+        className="w-full h-full"
         alt="사진"
         src={src}
-        width={80}
-        height={80}
+        width={700}
+        height={475}
+        sizes="100vw"
         quality={80}
+        priority
       />
     </div>
   );
